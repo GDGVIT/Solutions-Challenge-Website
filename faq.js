238fde9cbd7729e932b2ce1dd65eb767bd45ef61
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-02-05 00:16:44 +0530

    integrated form and made changes in validation

--- faq.js
+++ faq.js
@@ -2,4 +2,18 @@ $(".question").on('click',function(){
 	$(this).children('.answer').slideToggle();
 	$('.fa-angle-up',this).toggleClass('hideFont');
 	$('.fa-angle-down',this).toggleClass('hideFont');
+	// if(!$('.question').children('.answer').hasClass('hide')){
+		
+	// }
 })
+
+// function isOpen(question){
+// 	if($(question).hasClass('hide'))
+// 	{
+// 		return true;
+// 	}
+// 	else
+// 	{
+// 		return false;
+// 	}
+// }
